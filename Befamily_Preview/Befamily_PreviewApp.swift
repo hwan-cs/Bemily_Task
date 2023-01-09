f22ab92062724fcf16b41353798a26bb0b78d43c
Jung Hwan Park <hwann1005@gmail.com>
2023-01-09 16:52:12 +0900

    Initial Commit

---
+++ Befamily_Preview/Befamily_PreviewApp.swift
@@ -0,0 +1,20 @@
+//
+//  Befamily_PreviewApp.swift
+//  Befamily_Preview
+//
+//  Created by Jung Hwan Park on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct Befamily_PreviewApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
